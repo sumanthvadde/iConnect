961832c7ccbe200bcff80832d3b61056500c70dd
Sumanth Reddy <sumanth368@gmail.com>
2023-09-29 14:57:39 -0700

    Add files via upload

---
+++ client/src/actions/auth.js
@@ -0,0 +1,69 @@
+import { setAlert } from "./alert";
+import api from "../utils/api";
+
+import {
+	REGISTER_FAIL,
+	REGISTER_SUCCESS,
+	USER_LOADED,
+	AUTH_ERROR,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	LOGOUT,
+} from "./types";
+
+export const loadUser = () => async (dispatch) => {
+	try {
+		const res = await api.get("/auth");
+		dispatch({
+			type: USER_LOADED,
+			payload: res.data,
+		});
+	} catch (err) {
+		dispatch({
+			type: AUTH_ERROR,
+		});
+	}
+};
+
+export const register = (formData) => async (dispatch) => {
+	try {
+		const res = await api.post("/users", formData);
+		dispatch({
+			type: REGISTER_SUCCESS,
+			payload: res.data,
+		});
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		}
+
+		dispatch({
+			type: REGISTER_FAIL,
+		});
+	}
+};
+
+export const login = (email, password) => async (dispatch) => {
+	const body = { email, password };
+	try {
+		const res = await api.post("/auth", body);
+		dispatch({
+			type: LOGIN_SUCCESS,
+			payload: res.data,
+		});
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		}
+
+		dispatch({
+			type: LOGIN_FAIL,
+		});
+	}
+};
+
+export const logout = () => ({ type: LOGOUT });
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,12 @@
+import api from "./api";
+
+const setAuthToken = (token) => {
+	if (token) {
+		api.defaults.headers.common["x-auth-token"] = token;
+	} else {
+		delete api.defaults.headers.common["x-auth-token"];
+		localStorage.removeItem("token");
+	}
+};
+
+export default setAuthToken;
